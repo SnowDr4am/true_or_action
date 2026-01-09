1b1a1d96276ca17d50f3d440d138e0a526bbe473
SnowDr4am <snowdream.va@gmail.com>
2026-01-09 23:14:28 +0500

    ui fix: delete button start game when room hasnt status WAITING

--- app/keyboards/user/start.py
+++ app/keyboards/user/start.py
@@ -1,6 +1,6 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from app.database.models import User
+from app.database.models import User, RoomStatus
 from ..general import delete_message_keyboard, generate_simple_keyboard
 
 
@@ -9,11 +9,11 @@ def generate_start_menu(user: User) -> InlineKeyboardMarkup:
 
     if user.room:
         room = user.room
-
         is_owner = room.owner_id == user.telegram_id
 
         if is_owner:
-            buttons.append([InlineKeyboardButton(text="▶️ Начать игру", callback_data=f"room:start_game:{room.id}")])
+            if room.status == RoomStatus.WAITING:
+                buttons.append([InlineKeyboardButton(text="▶️ Начать игру", callback_data=f"room:start_game:{room.id}")])
             buttons.append([InlineKeyboardButton(text="🔒 Удалить комнату", callback_data=f"room:close:{room.id}")])
         else:
             buttons.append([InlineKeyboardButton(text="🚪 Покинуть комнату", callback_data="room:leave")])
